17cc2c72754d0dd1418e6f982da4dd53cb2050be
Brant Robertson <brant@ucsc.edu>
2020-07-22 15:50:52 -0700

    Math matches Christopher with same header

--- check_sip.py
+++ check_sip.py
@@ -21,36 +21,54 @@ header['NAXIS'] = 2
 header['WCSAXES'] = 2
 #print(header['A_ORDER'])
 
-A_p_q = Apq_sip(header)
-B_p_q = Bpq_sip(header)
+print('A02 = ',header['A_0_2'])
+
+#A_p_q = Apq_sip(header)
+#B_p_q = Bpq_sip(header)
 
 #print(A_p_q[1,0],A_p_q[0,1],A_p_q[5,0])
 #print(B_p_q[1,0],B_p_q[0,1])
 
-CDi_j = CDij_sip(header)
+#CDi_j = CDij_sip(header)
 #print(CDi_j[0,0])
 #print(CDi_j[0,1])
 #print(CDi_j[1,0])
 #print(CDi_j[1,1])
+
+header['CRVAL1'] = 53.181671732945411 
+header['CRVAL2'] = -27.823842206235913 
 CRPIX1 = header['CRPIX1']
 CRPIX2 = header['CRPIX2']
 CRVAL1 = header['CRVAL1']
 CRVAL2 = header['CRVAL2']
+ 
 
+CRPIX = np.array([CRPIX1,CRPIX2],dtype=np.float64)
 print("CRPIX = ",CRPIX1, CRPIX2)
 print("CRVAL = ",CRVAL1, CRVAL2)
 
 xy_off = [header['CRVAL1'],header['CRVAL2']]
-xy = xy_from_sip(0.,0.,header) + xy_off
-print("For uv=[0,0], xy=",xy,xy_off)
+#xy = xy_from_sip(0.,0.,header) + xy_off
+#print("For uv=[0,0], xy=",xy,xy_off)
+
+xy_det = np.array([1,1],dtype=np.float64)
+uv = xy_det.copy() - CRPIX
+#print("First, xy_det = ",xy_det)
+#print("First, uv = ",uv)
+#xy = xy_from_sip(uv[0],uv[1], header) + xy_off
+#print("For uv=[-1023.5,-1023.5], xy=",xy)
+
 xy = xy_from_sip(1.,1.,header) + xy_off
-print("For uv=[1,1], xy=",xy)
+s = "For uv=[%f,%f], xy= [%13.11f, %13.11f]" % (uv[0],uv[1],xy[0],xy[1])
+print(s)
 
 #print(header)
 
-pixcrd = np.array([[CRPIX1-0.5,CRPIX2-0.5],[CRPIX1+0.5,CRPIX2+0.5]])
+xy_det = [[1.0,1.0],[CRPIX1+1,CRPIX2+1]]
+print("Check xy_det = ",xy_det)
+pixcrd = np.array(xy_det,dtype=np.float64)
 w = wcs.WCS(header)
-world = w.all_pix2world(pixcrd, 0)[0]
+world = w.all_pix2world(pixcrd, 0)
 print(world)
 
 
--- sip_transform.py
+++ sip_transform.py
@@ -5,10 +5,11 @@ def xy_from_sip(u, v, header):
 
 	#define the vector (u,v)
 	uv = np.array([u,v],dtype=np.float64)
-	print(uv)
+	print("uu,vv = ",uv)
 
 	#get the A_p_q from the header
 	A_p_q = Apq_sip(header)
+	print(A_p_q)
 
 	#get the B_p_q from the header
 	B_p_q = Bpq_sip(header)
@@ -18,16 +19,31 @@ def xy_from_sip(u, v, header):
 	print("CDij = ",CDi_j)
 
 	#add the f(u,v) and g(u,v) functions
-	uv[0] += fuv_sip(u,v,A_p_q) # u + f(u,v)
-	uv[1] += guv_sip(u,v,B_p_q) # v + g(u,v)
+	fuv = fuv_sip(u,v,A_p_q)
+	guv = guv_sip(u,v,B_p_q)
+	print("xsum, ysum = ",fuv,guv)
+
+	uv[0] += fuv # u + f(u,v)
+	uv[1] += guv # v + g(u,v)
 
 	#evaluate the transform
-	print(uv)
-	xy = np.dot(CDi_j, uv)
-	print(xy)
+	print("xx = uu + xsum ",uv[0])
+	print("yy = vv + ysum ",uv[1])
 
-	print(CDi_j[0,0]*uv[0] + CDi_j[0,1]*uv[1])
-	print(CDi_j[1,0]*uv[0] + CDi_j[1,1]*uv[1])
+	xy = np.dot(CDi_j, uv)
+
+
+
+	#xy[0]/=np.cos(xy[0]*np.pi/180.0)
+	cosdec = np.cos(header['CRVAL2']*np.pi/np.double(180.0))
+	print(cosdec)
+	xy[0]/=cosdec
+	print(type(xy[0]))
+	s = "%13.10e %13.10e" % (xy[0],xy[1])
+	print(s)
+
+	#print((CDi_j[0,0]*uv[0] + CDi_j[0,1]*uv[1])/np.cos(header['CRVAL1']*np.pi/180.0) )
+	#print(CDi_j[1,0]*uv[0] + CDi_j[1,1]*uv[1])
 
 	return xy
 
@@ -68,10 +84,17 @@ def Bpq_sip(header):
 
 def CDij_sip(header):
 	CDi_j = np.zeros((2,2),dtype=np.float64)
-	CDi_j[0,0] = header['CD1_1']
-	CDi_j[0,1] = header['CD1_2']
-	CDi_j[1,0] = header['CD2_1']
-	CDi_j[1,1] = header['CD2_2']
+	#CDi_j[0,0] = np.double(header['CD1_1'])
+	#CDi_j[0,1] = np.double(header['CD1_2'])
+	#CDi_j[1,0] = np.double(header['CD2_1'])
+	#CDi_j[1,1] = np.double(header['CD2_2'])
+	CDi_j[0,0] = np.double(-2.114214532367e-4)
+	CDi_j[0,1] = np.double(1.801706494922e-4)
+	CDi_j[1,0] = np.double(1.801706494922e-4)
+	CDi_j[1,1] = np.double(2.114214532367e-4)
+	#print(CDi_j)
+
+	#print(type(CDi_j[0,0]))
 
 	return CDi_j
 
@@ -82,7 +105,7 @@ def fuv_sip(u,v,A_p_q):
 	print(A_p_q.shape[0],A_p_q.shape[1])
 
 	for p in range(A_p_q.shape[0]):
-		for q in range(A_p_q.shape[1]):
+		for q in range(A_p_q.shape[1]-p):
 			fuv += A_p_q[p,q]*(u**p)*(v**q)
 
 	return fuv
@@ -93,7 +116,7 @@ def guv_sip(u,v,B_p_q):
 	print(B_p_q.shape[0],B_p_q.shape[1],u,v)
 
 	for p in range(B_p_q.shape[0]):
-		for q in range(B_p_q.shape[1]):
+		for q in range(B_p_q.shape[1]-p):
 			guv += B_p_q[p,q]*(u**p)*(v**q)
 
 	return guv
